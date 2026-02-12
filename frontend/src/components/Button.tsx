220789e5059649e289340df39ac5672f3b3c0ecf
PavaneeshwarReddy <pavaneeshwar7077@gmail.com>
2026-02-12 22:10:54 +0530

    Added border radius to full

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -6,7 +6,7 @@ type ButtonType = {
 
 function Button({placeholder, width="fit"}: ButtonType) {
     return (
-        <div className={`p-4 bg-black text-white rounded-full w-${width} cursor-pointer text-center`}>
+        <div className={`p-4 bg-black text-white w-${width} rounded-full cursor-pointer text-center`}>
             {placeholder}
         </div>
     )
--- frontend/src/features/home/pages/Home.tsx
+++ frontend/src/features/home/pages/Home.tsx
@@ -6,10 +6,12 @@ function Home() {
     <div className='min-h-screen overflow-hidden h-screen flex flex-col justify-center'>
       <div className='flex lg:flex-row flex-col-reverse justify-between items-center'>
         <div className='space-y-5 lg:w-3/5 w-full'>
-          <div className='text-5xl '>Building  systems & <br />
+          <div className='text-5xl'>Building  systems & <br />
             understanding how they work</div>
           <div className='font-code text-lg'>A space where I share what I build, break, and learn along the way.</div>
-          <Button placeholder="Start reading" width="fit"/>
+          <div className="lg:w-1/4 w-full">
+            <Button placeholder="Start reading" width="fit" />
+          </div>
         </div>
         <div className="grid grid-cols-3 gap-3 items-stretch justify-center lg:w-2/5 w-full h-full">
           <div className="flex flex-col justify-center items-center h-full bg-black text-white">
